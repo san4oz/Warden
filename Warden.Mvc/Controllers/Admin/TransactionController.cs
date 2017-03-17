1041313a35599101cd34829a29b35b8661f54a6d
kotvytskyi <another0ne@mail.ru>
2017-03-17 18:21:31 +0200

    NLP helpers

--- Warden.Mvc/Controllers/Admin/TransactionController.cs
+++ Warden.Mvc/Controllers/Admin/TransactionController.cs
@@ -8,6 +8,7 @@ using Warden.Business.Contracts.Providers;
 using Warden.Business.Entities.ExternalProvider;
 using Warden.Search.Utils.Tokenizer;
 using Warden.Mvc.Helpers;
+using Warden.Core.NLP;
 
 namespace Warden.Mvc.Controllers.Admin
 {
@@ -24,13 +25,25 @@ namespace Warden.Mvc.Controllers.Admin
 
         public ActionResult List()
         {
+            var path = @"C:\Users\o.kotvytskyi\Documents\Visual Studio 2015\Projects\Warden-Git\Warden\Warden.Web\App_Data\ukrainian.dic";
+
             var transactions = Provider.GetTransactions(new TransactionRequest());
 
             var tokenizer = new SimpleWordTokenizer();
-
-            foreach(var transaction in transactions)
+            var stemmer = new RussianStemmer();
+            var dictionary = System.IO.File.ReadAllLines(path).ToList();
+           
+            foreach (var transaction in transactions)
             {
-                transaction.Keywords = string.Join(";", tokenizer.Tokenize(transaction.Keywords));
+                transaction.Keywords = string.Join(";", tokenizer.Tokenize(transaction.Keywords)
+                                         .Select(w =>
+                                         {
+                                             stemmer.Stem(w);
+                                             return WordHelper.RemoveSuffix(w);
+                                         }));
+
+
+
                 TransactionProvider.Save(transaction);
             }
 
