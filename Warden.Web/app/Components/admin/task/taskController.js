2ab38568eda9aadd4c4d2b4e372ca920b002a252
Arsendiabolikkys <diabolikkys@gmail.com>
2017-03-27 12:00:46 +0300

    Search and task functionality modified

---
+++ Warden.Web/app/Components/admin/task/taskController.js
@@ -0,0 +1,19 @@
+﻿'use strict'
+
+adminApp.controller('taskController', function ($scope, taskService) {
+    //taskService.getAll().then(function (result) {
+    //    $scope.tasks = result.data;
+    //});
+
+    $scope.indexTransactions = function () {
+        taskService.indexTransactions()
+            .then(function (result) {
+                alert("Виконано!");
+            });
+    };
+
+    $scope.runTask = function () {
+        //run specific task
+        $scope.indexTransactions();
+    };
+});
